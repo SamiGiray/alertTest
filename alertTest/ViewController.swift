0a50d85b39db46dea0a0570488254fdfc2e5c7e5
SamiGiray <samigiray@gmail.com>
2022-06-23 12:17:28 +0300

    Initial Commit

---
+++ alertTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alertTest
+//
+//  Created by Sami Giray Doğrultucu on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
